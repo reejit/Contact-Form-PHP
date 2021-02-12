1337d693536cb6b24286b5c2d1cd3cc04045efa8
reejit <70316178+reejit@users.noreply.github.com>
2021-02-12 15:42:19 +0530

    Update AjaxForm.php

--- AjaxForm.php
+++ AjaxForm.php
@@ -22,6 +22,81 @@ require __DIR__ . '/vendor/PHPMailer/PHPMailer.php';
 require __DIR__ . '/vendor/PHPMailer/SMTP.php';
 require __DIR__ . '/vendor/recaptcha/autoload.php';
 
+//Dectect OS and browser
+    $user_agent = $_SERVER['HTTP_USER_AGENT'];
+
+    function getOS() { 
+    
+        global $user_agent;
+    
+        $os_platform  = "Unknown OS Platform";
+    
+        $os_array     = array(
+                              '/windows nt 10/i'      =>  'Windows 10',
+                              '/windows nt 6.3/i'     =>  'Windows 8.1',
+                              '/windows nt 6.2/i'     =>  'Windows 8',
+                              '/windows nt 6.1/i'     =>  'Windows 7',
+                              '/windows nt 6.0/i'     =>  'Windows Vista',
+                              '/windows nt 5.2/i'     =>  'Windows Server 2003/XP x64',
+                              '/windows nt 5.1/i'     =>  'Windows XP',
+                              '/windows xp/i'         =>  'Windows XP',
+                              '/windows nt 5.0/i'     =>  'Windows 2000',
+                              '/windows me/i'         =>  'Windows ME',
+                              '/win98/i'              =>  'Windows 98',
+                              '/win95/i'              =>  'Windows 95',
+                              '/win16/i'              =>  'Windows 3.11',
+                              '/macintosh|mac os x/i' =>  'Mac OS X',
+                              '/mac_powerpc/i'        =>  'Mac OS 9',
+                              '/linux/i'              =>  'Linux',
+                              '/ubuntu/i'             =>  'Ubuntu',
+                              '/iphone/i'             =>  'iPhone',
+                              '/ipod/i'               =>  'iPod',
+                              '/ipad/i'               =>  'iPad',
+                              '/android/i'            =>  'Android',
+                              '/blackberry/i'         =>  'BlackBerry',
+                              '/webos/i'              =>  'Mobile'
+                        );
+    
+        foreach ($os_array as $regex => $value)
+            if (preg_match($regex, $user_agent))
+                $os_platform = $value;
+    
+        return $os_platform;
+    }
+    
+    function getBrowser() {
+    
+        global $user_agent;
+    
+        $browser        = "Unknown Browser";
+    
+        $browser_array = array(
+                                '/msie/i'      => 'Internet Explorer',
+                                '/firefox/i'   => 'Firefox',
+                                '/safari/i'    => 'Safari',
+                                '/chrome/i'    => 'Chrome',
+                                '/edge/i'      => 'Edge',
+                                '/opera/i'     => 'Opera',
+                                '/netscape/i'  => 'Netscape',
+                                '/maxthon/i'   => 'Maxthon',
+                                '/konqueror/i' => 'Konqueror',
+                                '/mobile/i'    => 'Handheld Browser'
+                         );
+    
+        foreach ($browser_array as $regex => $value)
+            if (preg_match($regex, $user_agent))
+                $browser = $value;
+    
+        return $browser;
+    }
+    
+    
+    $user_os        = getOS();
+    $user_browser   = getBrowser();
+    
+    $device_details = "Browser: ".$user_browser."\n OS: ".$user_os."";
+    $ip = $ip = getenv('HTTP_CLIENT_IP');
+
 class Ajax_Form {
     
     # Constants to redefined
@@ -47,12 +122,16 @@ class Ajax_Form {
             <p><strong>Name :</strong> {{name}}</p>
             <p><strong>E-Mail :</strong> {{email}}</p>
             <p><strong>Message :</strong> {{message}}</p>
+            <p><strong>$device_details :$ip</strong></p>
         ',
     ];
 
     /**
      * Ajax_Form constructor
+
      */
+
+
     public function __construct() {
 
         # Check if request is Ajax request
